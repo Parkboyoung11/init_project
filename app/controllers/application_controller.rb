218f4971712226a19ff1fbaafa708dad88133b53
Parkboyoung11 <vuhongsonjv11@gmail.com>
2018-07-09 10:39:46 +0700

    Finish Static Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def default_url_options
+    {locale: i18n.locale}
+  end
+
+  private
+
+  def set_locale
+    i18n.locale = params[:locale] || i18n.default_locale
+  end
 end
