c4ee973ac52de76fc2fbdc76a06fa2b35a015918
Parkboyoung11 <vuhongsonjv11@gmail.com>
2018-07-11 02:35:15 +0700

    Chap 8 + 9 done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  protect_from_forgery with: :exception
+  include SessionsHelper
 
   def default_url_options
     {locale: I18n.locale}
